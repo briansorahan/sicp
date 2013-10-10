3773a01067453e9a44cd6a256c6cbc3941c2ee6a
Brian Sorahan <bsorahan@gmail.com>
2013-10-09 21:20:05 -0500

    Added gitignore and ex1.10

---
+++ ch1/ex1.10.scm
@@ -0,0 +1,37 @@
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(define (f n) (A 0 n))
+(define (g n) (A 1 n))
+(define (h n) (A 2 n))
+
+;; in mathematical terms,
+;;
+;; f(x) = 2x
+;;
+;;        |-- 0 if x = 0
+;; g(x) = |
+;;        |-- 2^x otherwise
+;;
+;;        |-- 0 if x = 0
+;;        |
+;; h(x) = |-- 2 if x = 1
+;;        |
+;;        |-- 2^(x+1) otherwise
+;;
+;; (h 2) is
+;; (A 2 2)
+;; (A 1 (A 2 1))
+;; (A 1 2)
+;; (A 0 (A 1 1))
+;; (A 0 2)
+;; 4
+;;
+;; (h 3) is
+;; (A 2 3)
+;; (A 1 (A 2 2))
+;; (A
